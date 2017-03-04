d48caca5350a814ed896b56d214cee7dc30e2621
Filip Lejhanec <emb15144@ds.strath.ac.uk>
2017-03-04 14:48:23 +0000

    Deleted controller

--- src/Kamisado.java
+++ src/Kamisado.java
@@ -3,54 +3,10 @@ import java.util.Set;
 
 /**
  * Filip Lejhanec
- * Fraser Steel */public class Kamisado {
-/**
-    * <pre>
-    *           0..1     0..*
-    * Kamisado ------------------------- Data
-    *           kamisado        &gt;       data
-    * </pre>
-    */
-   private Set<Data> data;
-   
-   public Set<Data> getData() {
-      if (this.data == null) {
-         this.data = new HashSet<Data>();
-      }
-      return this.data;
-   }
-   
-   /**
-    * <pre>
-    *           0..*     0..*
-    * Kamisado ------------------------> UI
-    *           kamisado        &gt;       uI
-    * </pre>
-    */
-   private Set<UI> uI;
-   
-   public Set<UI> getUI() {
-      if (this.uI == null) {
-         this.uI = new HashSet<UI>();
-      }
-      return this.uI;
-   }
-   
-   /**
-    * <pre>
-    *           0..*     0..*
-    * Kamisado ------------------------> Game
-    *           kamisado        &gt;       game
-    * </pre>
-    */
-   private Set<Game> game;
-   
-   public Set<Game> getGame() {
-      if (this.game == null) {
-         this.game = new HashSet<Game>();
-      }
-      return this.game;
-   }
+ * Fraser Steel 
+ */
+
+public class Kamisado {
    
    public void newGame() {
       // TODO implement this operation
@@ -85,9 +41,8 @@ import java.util.Set;
    
 
 	public static void main(String[] args) {
-		Game game = new Game();
-		game.start();
-
+		Data data = new Data("Kamisado.data");
+		UI ui = new UI();
+		ui.displayMenu();
 	}
-
 }
--- src/UI.java
+++ src/UI.java
@@ -2,30 +2,9 @@ import java.util.Set;
 import java.util.HashSet;
 
 public class UI {
-   /**
-    * <pre>
-    *           0..*     0..*
-    * UI ------------------------- Controller
-    *           uI        &gt;       controller
-    * </pre>
-    */
-   private Set<Controller> controller;
    
-   public Set<Controller> getController() {
-      if (this.controller == null) {
-         this.controller = new HashSet<Controller>();
-      }
-      return this.controller;
+   public void ui.displayMenu(){
+	   System.out.println("New Game");
+	   System.out.println("New Game");
    }
-   
-   public void render() {
-      // TODO implement this operation
-      throw new UnsupportedOperationException("not implemented");
-   }
-   
-   public void getInput() {
-      // TODO implement this operation
-      throw new UnsupportedOperationException("not implemented");
-   }
-   
-   }
+}
