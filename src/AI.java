8a48b46c164555a13f6379ca9dd5fc4427266180
Filip Lejhanec <emb15144@uni.strath.ac.uk>
2017-04-11 11:29:25 +0100

    AI now differs whose turn it is, fixed sumopush AI bugs

--- src/AI.java
+++ src/AI.java
@@ -1,9 +1,12 @@
 import java.util.List;
 public final class AI {
+	
+	//bottom player minimising, top player maximising
    public static Move MiniMaxAB(Board board, int depth){
 	   int a = -10000;
 	   int b = 10000;
 	   int value = 0;
+	   Value currentPlayerPos = board.getCurrentPlayerPosition();
 	   Board newBoard;
 	   Move bestMove = new Move(-1,-1,-1,-1);
 	   List<Move> moves = board.getValidMoves();
@@ -20,17 +23,31 @@ public final class AI {
 				   if(newBoard.isWinningMove(newBoard.getLastPlayerPosition(), move)) {
 					   return move;
 				   } else {
-					   value = MinAB(newBoard, depth - 1, a, b);
-					   if (value > a) {
-						   a = value;
-						   bestMove = move;
+					   if (newBoard.getCurrentPlayerPosition() == Value.BOTTOM) {
+						   value = MinAB(newBoard, depth - 1, a, b);
+					   } else if (newBoard.getCurrentPlayerPosition() == Value.TOP){
+						   value = MaxAB(newBoard, depth - 1, a, b);
 					   }
-					  
+					   if (currentPlayerPos == Value.TOP) {
+						   if (value > a) {
+							   a = value;
+							   bestMove = move;
+						   }
+					   } else if (currentPlayerPos == Value.BOTTOM){
+						   if (value < b) {
+							   b = value;
+							   bestMove = move;
+						   }
+					   }
 				   }
 			   }
 		   }
 	   }
-	   System.out.println(a);
+	   if (currentPlayerPos == Value.TOP) {
+		   System.out.println(a);
+	   } else if (currentPlayerPos == Value.BOTTOM){
+		   System.out.println(b);
+	   }
 	   return bestMove;
    }
    
@@ -42,9 +59,13 @@ public final class AI {
 		   Board newBoard;
 		   List<Move> moves = board.getValidMoves();
 		   if (moves.size() == 0) {
-			   newBoard = new Board(board);
-			   newBoard.makeMove(newBoard.getDeadLockMove());
-			   return MaxAB(newBoard, depth - 1, a, b);
+			   if (board.isGameOver() && board.getGameOverCause() == Value.DOUBLE_DEADLOCK){
+				   return -10*depth;
+			   } else {
+				   newBoard = new Board(board);
+				   newBoard.makeMove(newBoard.getDeadLockMove());
+				   return MaxAB(newBoard, depth - 1, a, b);
+			   }
 		   } else {
 			   for (int i = 0; i < moves.size(); i++){
 				   Move move = moves.get(i);
@@ -53,7 +74,11 @@ public final class AI {
 				   if(newBoard.isWinningMove(newBoard.getLastPlayerPosition(), move)) {
 					   return -10*depth;
 				   } else {
-					   value = MaxAB(newBoard, depth - 1, a, b);
+					   if (newBoard.getCurrentPlayerPosition() == Value.BOTTOM) {
+						   value = MinAB(newBoard, depth - 1, a, b);
+					   } else if (newBoard.getCurrentPlayerPosition() == Value.TOP){
+						   value = MaxAB(newBoard, depth - 1, a, b);
+					   }
 					   
 					   if (value <= a) {
 						   return a;
@@ -77,9 +102,13 @@ public final class AI {
 		   Board newBoard;
 		   List<Move> moves = board.getValidMoves();
 		   if (moves.size() == 0) {
-			   newBoard = new Board(board);
-			   newBoard.makeMove(newBoard.getDeadLockMove());
-			   return MinAB(newBoard, depth - 1, a, b);
+			   if (board.isGameOver() && board.getGameOverCause() == Value.DOUBLE_DEADLOCK){
+				   return 10*depth;
+			   } else {
+				   newBoard = new Board(board);
+				   newBoard.makeMove(newBoard.getDeadLockMove());
+				   return MinAB(newBoard, depth - 1, a, b);
+			   }
 		   } else {
 			   for (int i = 0; i < moves.size(); i++){
 				   Move move = moves.get(i);
@@ -88,7 +117,11 @@ public final class AI {
 				   if(newBoard.isWinningMove(newBoard.getLastPlayerPosition(), move)) {
 					   return 10*depth;
 				   } else {
-					   value = MinAB(newBoard, depth - 1, a, b);
+					   if (newBoard.getCurrentPlayerPosition() == Value.BOTTOM) {
+						   value = MinAB(newBoard, depth - 1, a, b);
+					   } else if (newBoard.getCurrentPlayerPosition() == Value.TOP){
+						   value = MaxAB(newBoard, depth - 1, a, b);
+					   }
 					   
 					   if (value >= b) {
 						   return b;
